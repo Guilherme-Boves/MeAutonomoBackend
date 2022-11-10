32741cd94e0ae8f46fd0a2dbda0f74c10931b669
Guilherme Boves <ghboves@gmail.com>
2022-11-10 20:53:02 -0300

    correção de bug no cancelamento do serviço

--- src/controllers/servicos/CancelarServicoController.ts
+++ src/controllers/servicos/CancelarServicoController.ts
@@ -4,10 +4,12 @@ import { CancelarServicoService } from "../../services/servicos/CancelarServicoS
 class CancelarServicoController {
     async handle(req: Request, res: Response){
 
-        const { contrato_id, itemContrato_id, agenda_id } = req.body;
+        const contrato_id = req.query.contrato_id as string; 
+        const itemContrato_id = req.query.itemContrato_id as string;
+        const agenda_id = req.query.agenda_id as string;
 
         const cancelarServicoService = new CancelarServicoService();
-
+        
         const cancelarServico = await cancelarServicoService.execute({
             contrato_id,
             itemContrato_id, 
